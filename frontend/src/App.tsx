d0d85408ed9e5395321f9f75b8c7c400c89ef9e8
is.ssj@mail.ru <is.ssj@mail.ru>
2026-03-03 12:39:43 +0300

    52

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ interface Item {
   type: string
   title: string
   created_at: string
+  description: string 
 }
 
 function App() {
@@ -94,6 +95,7 @@ function App() {
               <th>Type</th>
               <th>Title</th>
               <th>Created at</th>
+              <th>Description</th>
             </tr>
           </thead>
           <tbody>
